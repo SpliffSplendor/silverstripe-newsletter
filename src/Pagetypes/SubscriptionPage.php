4eeeb6cfa87f176a92ecf60705ee88d006d5239a
Spliff <Spliff.Splendor@gmail.com>
2020-11-09 14:13:45 -0600

    Fix errors for running on SS-CMS version 4.6
    also:
    - Missing js requirement for 'Send notification Email to subscriber'
    - Keep sequence of drag'n'drop order
    - Keep selected checkbox on CheckboxSetWithExtraFields
    - Show validation time for verification email in admin and add translation (en es)
    - improve documentation
    - Fix errors:
    [2020-11-04 16:03:37] error-log.ERROR: Uncaught Exception Error: "Call to a member function setTitle() on null" at /var/www/html/vendor/silverstripe/newsletter/src/Form/GridField/NewsletterGridFieldDetailForm_ItemRequest.php line 32 {"exception":"[object] (Error(code: 0): Call to a member function setTitle() on null at /var/www/html/vendor/silverstripe/newsletter/src/Form/GridField/NewsletterGridFieldDetailForm_ItemRequest.php:32)"} []
    
    Problem identified was the naming of the src/Form/GridField/NewsletterGridFieldDetailForm_ItemRequest.php line 31:
    $actions->fieldByName("action_doSave") this did return null and didn't initiate a recursive search.
    $actions->fieldByName("MajorActions.action_doSave") does.
    
    [2020-11-05 17:24:28] error-log.ERROR: Object->__call(): the method 'addCustomHeader' does not exist on 'SilverStripe\Newsletter\Control\Email\NewsletterEmail' {"exception":"[object] (BadMethodCallException(code: 0): Object->__call(): the method 'addCustomHeader' does not exist on 'SilverStripe\\Newsletter\\Control\\Email\\NewsletterEmail' at  vendor/silverstripe/framework/src/Core/CustomMethods.php:54)"} []
    
    Method addCustomHeader is not part of the NewsletterEmail class or its parents, called in src/Model/SendRecipientQueue.php line 82
        $email->addCustomHeader('Reply-To', $newsletter->ReplyTo);
    Because it's the only occurrence and is used for setting the Reply-To header, we can use
        $email->setReplyTo($newsletter->ReplyTo);
    instead.

--- src/Pagetypes/SubscriptionPage.php
+++ src/Pagetypes/SubscriptionPage.php
@@ -2,6 +2,8 @@
 
 namespace SilverStripe\Newsletter\Pagetypes;
 
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\FormField;
 use SilverStripe\Forms\Tab;
 use SilverStripe\Forms\HeaderField;
 use SilverStripe\Forms\TextField;
@@ -15,7 +17,25 @@ use SilverStripe\Newsletter\Model\MailingList;
 use SilverStripe\Newsletter\Form\CheckboxSetWithExtraField;
 use Page;
 use SilverStripe\Newsletter\Control\NewsletterAdmin;
+use SilverStripe\View\Requirements;
 
+/**
+ * Class SubscriptionPage
+ * @package SilverStripe\Newsletter\Pagetypes
+ *
+ * @property string $Fields    comma separated list of selected fields
+ * @property string $Required  JSON list of required fields e.g. { "Email":"1" }
+ * @property string $CustomisedHeading  Text before the subscription form
+ * @property string $CustomLabel    JSON Customized labels for the fields e.g. { "Email": "Email address" }
+ * @property string $ValidationMessage  JSON customized validation message for fields
+ *                                      e.g. { "Email": "An email address is required" }
+ * @property string $MailingLists   JSON list of available mailing list IDs for this subscription e.g. { "1", "5" }
+ * @property string $SubmissionButtonText Text to appear on the submit button (is not translated)
+ * @property bool $SendNotification     send a notification email to subscriber
+ * @property string $NotificationEmailSubject   Subject of the notification email
+ * @property string $NotificationEmailFrom  Sender-Email for the notification email
+ * @property string $OnCompleteMessage  Text to be shown when subscription process is completed
+ */
 class SubscriptionPage extends Page
 {
     private static $db = [
@@ -64,6 +84,17 @@ class SubscriptionPage extends Page
         }
     }
 
+    /**
+     * @return int configured number of days the subscription is valid
+     *             defaults to 2 if less then 2 or not set in config.
+     */
+    public static function get_days_verification_link_alive()
+    {
+        return SubscriptionPage::$days_verification_link_alive;
+    }
+    /**
+     * @InheritDoc
+     */
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
@@ -85,23 +116,35 @@ class SubscriptionPage extends Page
             new TextField('CustomisedHeading', 'Heading at the top of the form')
         );
 
-        //Fields selction
+        //Fields selection
+        /** @var array $frontFields [ fieldName => Field ] */
         $frontFields = singleton(Recipient::class)->getFrontEndFields()->dataFields();
 
-        $fieldCandidates = array();
+        $fieldCandidates = [];
+        // put all selected fields on top of the list of the available fields
+        $selectedFieldNames = explode(',', $this->Fields);
+        if (empty($selectedFieldNames)) {
+            $selectedFieldNames = ['Email'];
+        }
 
         if (count($frontFields)) {
+            foreach ($selectedFieldNames as $fieldName) {
+                if (array_key_exists($fieldName, $frontFields)) {
+                    $dataField = $frontFields[$fieldName];
+                    $fieldCandidates[$fieldName] = $dataField->Title()?$dataField->Title():$dataField->Name();
+                    unset($frontFields[$fieldName]);
+                    $values[$fieldName] = $fieldName;
+                }
+            }
             foreach ($frontFields as $fieldName => $dataField) {
                 $fieldCandidates[$fieldName]= $dataField->Title()?$dataField->Title():$dataField->Name();
             }
         }
 
         //Since Email field is the Recipient's identifier,
-        //and newsletters subscription is non-sence if no email is given by the user,
+        //and newsletters subscription is non-sense if no email is given by the user,
         //we should force that email to be checked and required.
-        //FisrtName should be checked as default, though it might not be required
-        $defaults = array("Email", "FirstName");
-
+        //FirstName should be checked as default, though it might not be required
         $extra = array('CustomLabel'=>"Varchar","ValidationMessage"=>"Varchar","Required" =>"Boolean");
         $extraValue = array(
             'CustomLabel'=>$this->CustomLabel,
@@ -109,17 +152,17 @@ class SubscriptionPage extends Page
             "Required" =>$this->Required
         );
 
-        $subscriptionTab->push(
-            $fieldsSelection = new CheckboxSetWithExtraField(
-                "Fields",
-                _t('Newsletter.SelectFields', "Select the fields to display on the subscription form"),
-                $fieldCandidates,
-                $extra,
-                $defaults,
-                $extraValue
-            )
+        $fieldsSelection = new CheckboxSetWithExtraField(
+            "Fields",
+            _t('Newsletter.SelectFields', "Select the fields to display on the subscription form"),
+            $fieldCandidates,
+            $selectedFieldNames,
+            $extra,
+            $extraValue
         );
 
+        $subscriptionTab->push($fieldsSelection);
+        // Email cannot be changed and is always required
         $fieldsSelection->setCellDisabled(array("Email"=>array("Value", "Required")));
 
         //Mailing Lists selection
@@ -153,6 +196,20 @@ class SubscriptionPage extends Page
 
         $subscriptionTab->push(
             new LiteralField(
+                'DaysVerificationIsValid',
+                sprintf(
+                    '<div id="DaysVerificationIsValid">'.
+                    _t('Newsletter.DaysVerificationIsValid', 'Validation for verification email is %d days').
+                    '<br/></div>',
+                    self::$days_verification_link_alive
+                )
+            )
+        );
+
+        Requirements::javascript('silverstripe/newsletter:client/javascript/SubscriptionPage.js');
+        Requirements::css('silverstripe/newsletter:client/css/SubscriptionPage.css');
+        $subscriptionTab->push(
+            new LiteralField(
                 'BottomTaskSelection',
                 sprintf(
                     '<div id="SendNotificationControlls" class="field actions">'.
@@ -166,6 +223,7 @@ class SubscriptionPage extends Page
                 )
             )
         );
+
 
         $subscriptionTab->push(
             CompositeField::create(
@@ -197,9 +255,55 @@ class SubscriptionPage extends Page
      * Email field is the member's identifier, and newsletters subscription is
      * non-sense if no email is given by the user, we should force that email
      * to be checked and required.
+     * @return string   JSON like {"Email":"1", "Surname": "1"}
      */
     public function getRequired()
     {
         return (!$this->getField('Required')) ? '{"Email":"1"}' : $this->getField('Required');
+    }
+
+    /**
+     * @param FieldList $frontEndFields FieldList of fields to be shown at the front-end.
+     *                                  This list are the fields for the Recipient without the excluded Fields
+     *
+     * @return FieldList    which contains only the selected fields for this Subscription Page in the selected order
+     */
+    public function getFrontendFieldList(FieldList  $frontEndFields)
+    {
+        if (empty($this->Fields)) {
+            $this->Fields = self::$defaults['Fields'];
+        }
+
+        $selectedFieldNames = explode(',', $this->Fields);
+        $selectedFrontEndFields = new FieldList();
+        foreach ($selectedFieldNames as $fieldName) {
+            $field = $frontEndFields->fieldByName($fieldName);
+            if ($field instanceof FormField) {
+                $selectedFrontEndFields->add($field);
+            }
+        }
+        return $selectedFrontEndFields;
+    }
+
+    /**
+     * @return array    list of required field names
+     *                  'Email' is always required and added to the list if missing
+     */
+    public function getRequiredFieldNames()
+    {
+        $requiredFieldNames = [];
+        $required = explode(',', trim($this->Required, '{}'));
+        foreach ($required as $requiredField) {
+            list($name, $one) = explode(':', $requiredField);
+            $name = trim($name, '"');
+            if (! empty($name)) {
+                $requiredFieldNames[] = $name;
+            }
+        }
+        // make sure that field Email is in the list
+        if (! in_array('Email', $requiredFieldNames)) {
+            $requiredFieldNames[] = 'Email';
+        }
+        return $requiredFieldNames;
     }
 }
